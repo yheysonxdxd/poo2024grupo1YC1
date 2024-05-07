4605ddcead01c36efd1d652149d588ac0480747f
yheysonxdxd <165050063+yheysonxdxd@users.noreply.github.com>
2024-05-07 08:52:46 -0500

    nuevoe

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/SysAlmacenVcApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.syscenterlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAlmacenVcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysAlmacenVcApplication.class, args);
+	}
+
+}
